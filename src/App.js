fa6bd39aaca80c37dc2c6ef3936d9fdfc2cdadaa
YifanM <yifan.ma@hotmail.com>
2017-02-11 21:11:58 -0500

    added shrinking, modal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import classnames from 'classnames';
 import './App.css';
-import Modal from 'react-modal';
+import {ModalContainer, ModalDialog} from 'react-modal-dialog';
 
 class PlayArea extends Component {
   render() {
-  	var areaClass = classnames({
-  		button: true,
-  		black: this.props.gameState === "b",
-  		white: this.props.gameState === "w",
-  	})
     return (
-      <button className={areaClass} onClick={() => this.props.onClick()}></button>
+    	<div id="playContainer">
+      	<button className="button" onClick={() => this.props.onClick()}></button>
+      	<PlayAreaGraphic colourState={this.props.colourState} gameState={this.props.gameState}/>
+      </div>
     );
   }
+}
+
+class PlayAreaGraphic extends Component {
+	render() {
+		var colour = classnames({
+			grow: this.props.gameState,
+			black: this.props.colourState === "b",
+			white: this.props.colourState === "w",
+		})
+		return (
+				<div className={colour}/>
+		);
+	}
 }
 
 class PlaceMarker extends Component {
@@ -32,13 +44,21 @@ class PlaceMarker extends Component {
 
 class Board extends Component {
   renderArea(r, c) {
-    return <PlayArea gameState={this.props.gameState[r][c]} value={[r, c]} onClick={() => this.props.onClick(r, c, "move")} class/>;
+    return <PlayArea colourState={this.props.colourState[r][c]} gameState={this.props.gameState[r][c]} onClick={() => this.props.onClick(r, c, "move")}/>;
+  }
+  componentDidMount() {
+  	var node = ReactDOM.findDOMNode(this);
+  	node.style.opacity = 0;
+  	window.requestAnimationFrame(function() {
+			node.style.transition = "opacity 2s";
+			node.style.opacity = 1;
+		});
   }
   render() {
     var gameBoard = [];
-    for (var i = 0; i < 17; i++){
+    for (var i = 0; i < 17; i++) {
       let row = [];
-      for (var j = 0; j < 17; j++){
+      for (var j = 0; j < 17; j++) {
         if ((i === 9 && j === 9) ||
             (i === 5 && j === 5) ||
             (i === 5 && j === 13) ||
@@ -65,27 +85,40 @@ class Game extends Component {
     super();
     this.state = {
       gameState: Array(17).fill(Array(17).fill(null)),
+      colourState: Array(17).fill(Array(17).fill(null)),
       gameHistory: [],
       blackMove: true,
       initialMoveBlack: true,
       winner: "",
       blackWins: 0,
       whiteWins: 0,
-    }
+      isModalOpen: false,
+    };
   }
-  handleClick(r, c, state){
+  handleClick(r, c, state) {
   	if ((this.state.gameState[r][c] || this.state.winner) && state === "move") return;
   	const gameState = this.state.gameState.map((row) => row.slice());
+  	const colourState = this.state.colourState.map((row) => row.slice());
   	const gameHistory = this.state.gameHistory.slice();
   	let blackWins = this.state.blackWins;
   	let whiteWins = this.state.whiteWins;
   	let result;
 
   	if (state === "move") { // move
-  		if (this.state.blackMove) gameState[r][c] = "b";
-	  	else gameState[r][c] = "w";
+  		result = moveResult(r, c, this.state.blackMove ? "b" : "w", gameState);
+  		if (result === "Illegal.") {
+  			//let players know somehow here
+  			return;
+  		}
+  		if (this.state.blackMove) {
+  			gameState[r][c] = "b";
+  			colourState[r][c] = "b";
+  		}
+	  	else {
+	  		gameState[r][c] = "w";
+	  		colourState[r][c] = "w";
+	  	}
 	  	gameHistory.push([r, c]);
-	  	result = moveResult(r, c, this.state.blackMove ? "b" : "w", gameState);
 	    if (result) result === "Black wins." ? blackWins += 1 : whiteWins += 1;	
   	} else { // undo
   		if (this.state.winner === "Black wins.") blackWins -= 1;
@@ -96,6 +129,7 @@ class Game extends Component {
 
     this.setState({
     	gameState: gameState,
+    	colourState: colourState,
     	gameHistory: gameHistory,
     	blackMove: !this.state.blackMove,
     	winner: result,
@@ -103,7 +137,7 @@ class Game extends Component {
     	whiteWins: whiteWins,
     });
   }
-  restartGame(){
+  restartGame() {
   	if (this.state.winner === "Black wins.") {
   		this.setState({
   			blackMove: false,
@@ -126,18 +160,30 @@ class Game extends Component {
   		gameHistory: [],
       winner: "",
   	});
+
   }
-  undoMove(){
+  undoMove() {
 		this.handleClick(this.state.gameHistory[this.state.gameHistory.length-1][0],
 										 this.state.gameHistory[this.state.gameHistory.length-1][1], "undo");
+  }
+  openModal() {
+  	this.setState({
+  		isModalOpen: true,
+  	})
+  }
+  closeModal() {
+  	this.setState({
+  		isModalOpen: false,
+  	})
   }
   render() {
     let status;
     if (this.state.winner) status = this.state.winner;
     else status = (this.state.blackMove ? "Black" : "White") + " to move.";
+
     return (
       <div>
-        <Board gameState={this.state.gameState} onClick={(r, c, state) => this.handleClick(r, c, state)}/>
+        <Board colourState={this.state.colourState} gameState={this.state.gameState} onClick={(r, c, state) => this.handleClick(r, c, state)}/>
         <div id="menu">
 	        <div>{status}</div>
 	        <button disabled={!this.state.gameHistory.length} onClick={() => this.undoMove()}>Undo</button>
@@ -147,21 +193,25 @@ class Game extends Component {
 	        <h4>Black's score: {this.state.blackWins}</h4>
 	        <h4>White's score: {this.state.whiteWins}</h4>
 	      </div>
-	      <button id="rules" onClick={() => this.openModal()}>Rules</button>
-	      {/*<Modal>
-        <h3>Rules</h3>
-        Each turn, select an intersection to play your piece. Rows can be made in any straight direction.
-        <ol>
-	        <li>White wins with rows of five or more.</li>
-	        <li>Black wins with only rows of five. In addition, Black cannot
-	        	<ol>
-	        		<li>a</li>
-	        		<li>list</li>
-	        		<li>here</li>
-	        	</ol>
-	        </li>
-        </ol>
-        </Modal>*/}
+	      <div id="rules">
+	      	<button id="rules" onClick={() => this.openModal()}>Rules</button>
+	      </div>
+	      {this.state.isModalOpen && <ModalContainer onClose={() => this.closeModal()}>
+	       	<ModalDialog onClose={() => this.closeModal()}>
+		        <h3>Rules</h3>
+		        Each turn, select an intersection to play your piece. Rows can be made in any straight direction.
+		        <ol>
+			        <li>Second player wins with rows of five or more.</li>
+			        <li>First player wins with only rows of five. In addition, first player cannot
+			        	<ol>
+			        		<li>a</li>
+			        		<li>list</li>
+			        		<li>here</li>
+			        	</ol>
+			        </li>
+		        </ol>
+		      </ModalDialog>
+        </ModalContainer>}
       </div>
     );
   }
@@ -175,29 +225,29 @@ class App extends Component {
   }
 }
 
-function travel(r, c, color, gameState, direction){
-  if (r < 0 || r > 16 || c < 0 || c > 16){
+function travel(r, c, color, gameState, direction) {
+  if (r < 0 || r > 16 || c < 0 || c > 16) {
     return 0;
   }
-  else if (gameState[r][c] === color){
+  else if (gameState[r][c] === color) {
     return travel(r+direction[0], c+direction[1], color, gameState, direction) + 1;
   }
   return 0;
 }
 
-function moveResult(r, c, color, gameState){
+function moveResult(r, c, color, gameState) {
   const directions = [[0, 1], [1, 1], [1, 0], [1, -1]];
-  for (let i = 0; i < 4; i++){
+  for (let i = 0; i < 4; i++) {
   	let reverse = directions[i].map(x => {return -x})
     let temp = travel(r+directions[i][0], c+directions[i][1], color, gameState, directions[i]) 
     			 	   + travel(r+reverse[0], c+reverse[1], color, gameState, reverse)
     	    		 + 1;
-    if (temp >= 5){
+    if (temp >= 5) {
   	  if (color === "b") return "Black wins.";
 	  	else return "White wins.";
     }
   }
-  //return "illegal";
+  //return "Illegal.";
   return null;
 }
 
